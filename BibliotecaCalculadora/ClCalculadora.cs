886ad8a3ce48e0045e107775d55f97a09622d883
Sergio Hervas <sergio@gmail.com>
2022-01-10 08:32:37 +0100

    penultim

--- BibliotecaCalculadora/ClCalculadora.cs
+++ BibliotecaCalculadora/ClCalculadora.cs
@@ -22,5 +22,130 @@ namespace BibliotecaCalculadora
             }
             return result;
         }
+        public int ProducteVectorial(int[] array1, int[] array2)
+        {
+            int resultat=0;
+            for (int i=0; i<array1.Length;i++)
+            {
+                resultat =  resultat + array1[i] * array2[i];
+
+            }
+            return resultat;
+
+        }
+
+        public int[] invertirVector(int[] array1)
+        {
+            /*int[] vResultat = new int[array1.Length];
+
+            for (int i= array1.Length-1, j=0; i>= 0;i--, j++)
+            {
+                    vResultat[j] = array1[i];
+            }
+            return vResultat;*/
+
+            Array.Reverse(array1);
+
+            return array1;
+        }
+
+        public char[] OrdenarVector(char[] array1)
+        {
+            Array.Sort(array1);
+
+            return array1;
+        }
+
+        public int buscarMaximo(int[] array1)
+        {
+            return array1.Max();
+        }
+
+        public int buscarMaximo2(int[] array1)
+        {
+            return array1.Max();
+        }
+        public int buscarMinimo(int[] array1)
+        {
+            return array1.Min();
+        }
+        
+       public int QuantesVegades(string cadena, char lletra)
+        {
+            int lletraTrobada = 0;
+
+            for (int i = 0; i < cadena.Length; i++)
+            {
+                /*if (cadena[i] != 0)
+                {
+                    while (cadena[i] != '\0')
+                    {*/
+                        if (cadena[i] == lletra)
+                        {
+                            lletraTrobada++;
+                        }
+                    //}
+                //}
+            }
+            return lletraTrobada;
+        }
+        
+        public int[] OnEs(string cadena, char lletra)
+        {
+            int [] indexPos = { };
+            int j = 0;
+
+            for (int i = 0; i < cadena.Length; i++)
+            {
+                        if (cadena[i] == lletra)
+                        {
+                            indexPos[j] = i;
+                            j++;
+                        }
+            }
+            return indexPos;
+        }
+        
+        public int QuantesVocals(string cadena)
+        {
+            int numVocals = 0;
+            char[] vocals = { 'a', 'e', 'i', 'o', 'u' };
+
+            for (int i = 0; i < cadena.Length; i++)
+            {
+               /*if (cadena[i] != null)
+                {
+                    while (cadena[i] != '\0')
+                    {*/
+                        for (int j = 0; j < vocals.Length; j++)
+                        {
+                            if (cadena[i] == vocals[j])
+                            {
+                                numVocals++;
+                            }
+                        }
+                    //}
+                //}
+            }
+            return numVocals;
+        }
+
+        public int QuantesConsonants(string cadena)
+        {
+            int numConsonants = 0;
+            char[] vocals = {'a','e','i','o','u'};
+
+            for (int i = 0; i < cadena.Length; i++)
+            {
+                for (int j = 0; j < vocals.Length; j++)
+                {
+                    if (cadena[i] != vocals[j])
+                    {
+                        numConsonants++;
+                    }
+                }
+            }
+            return numConsonants;
+        }
     }
 }
--- TestCalculadora/Test.cs
+++ TestCalculadora/Test.cs
@@ -1,6 +1,10 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using BibliotecaCalculadora;
 using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Linq;
+using System.Text;
 
 namespace TestCalculadora
 {
@@ -20,23 +24,128 @@ namespace TestCalculadora
         }
 
         [TestMethod]
-        public void ProvaMultiplicar()
+        public void producteVectorial()
         {
-            int resultat = 0;
-
+            int resultat;
+            int[] array1 = { 0, 1, 2, 3 };
+            int[] array2 = { 3, 2, 1, 0 };
             ClCalculadora kal = new ClCalculadora();
-            resultat = kal.Multiplicar(100000,100000);
-            Assert.AreEqual(1000000000, resultat);
+            resultat = kal.ProducteVectorial(array1, array2);
+            Assert.AreEqual(4, resultat);
+            Console.WriteLine("resultat: " + resultat);
 
         }
+            
         [TestMethod]
-        public void ProvaSumaVector()
+        public void invertirVector()
         {
-            int[] v1 = { 0, 13, 45, 37, 12 };
+            int[] resultat = { };
+            int[] array1 = {0,1,2,3};
+            int[] array2 = {3,2,1,0};
+            ClCalculadora kal = new ClCalculadora();
+            resultat = kal.invertirVector(array1);
+            CollectionAssert.AreEqual(array2, resultat);
+            Console.WriteLine("resultatt: " + resultat);
+        }
 
-            int[] v2 = { 0, 13, 45, 37, 12 };
+        [TestMethod]
+        public void ordenarVector()
+        {
+            char[] resultat = { };
+            char[] array1 = { 'd', 'c', 'b', 'a' };
+            char[] vectorOrdenat = { 'a', 'b', 'c', 'd' };
+            ClCalculadora kal = new ClCalculadora();
+            resultat = kal.OrdenarVector(array1);
+            CollectionAssert.AreEqual(vectorOrdenat, resultat);
+            Console.WriteLine("resultatt: " + resultat);
+        }
 
+        [TestMethod]
+        public void buscarMaximo()
+        {
+            int resultat;
+            int[] array1 = {1, 2, 3, 5};
+            int max = array1.Max();
+            ClCalculadora kal = new ClCalculadora();
+            resultat = kal.buscarMaximo(array1);
+            Assert.AreEqual(max, resultat);
+            Console.WriteLine("resultatt: " + resultat);
+        }
+        [TestMethod]
+        public void buscarMaximo2()
+        {
+            int resultat;
+            int[] array1 = { 1, 2, 3, 8 };
+            int max = array1.Max();
+            ClCalculadora kal = new ClCalculadora();
+            resultat = kal.buscarMaximo2(array1);
+            Assert.AreEqual(max, resultat);
+            Console.WriteLine("resultatt: " + resultat);
+        }
+        [TestMethod]
+        public void buscarMinimo()
+        {
+            int resultat;
+            int[] array1 = { 1, 5, 4, 8 };
+            int min = array1.Min();
+            ClCalculadora kal = new ClCalculadora();
+            resultat = kal.buscarMinimo(array1);
+            Assert.AreEqual(min, resultat);
+            Console.WriteLine("resultatt: " + resultat);
+        }
+        [TestMethod]
+        public void QuantesVegades()
+        {
+            int resultat;
+            String cadena = "hola tonto";
+            char lletra = 'o';
+            int countLletra = 3;
 
+            ClCalculadora kal = new ClCalculadora();
+            resultat = kal.QuantesVegades(cadena, lletra);
+            Assert.AreEqual(countLletra, resultat);
+            Console.WriteLine("resultatt: " + resultat);
+        }
+        
+        [TestMethod]
+        public void OnEs()
+        {
+            int[] resultat = { };
+            String cadena = "hola tonto";
+            char lletra = 'o';
+            int[] pos = {1,6,9};
+
+            ClCalculadora kal = new ClCalculadora();
+            resultat = kal.OnEs(cadena, lletra);
+            CollectionAssert.AreEqual(pos, resultat);
+            Console.WriteLine("resultatt: " + resultat);
+        }
+        
+        [TestMethod]
+        public void QuantesVocals()
+        {
+            int resultat;
+            String cadena = "hola tonto";
+
+            int numV = 4;
+
+            ClCalculadora kal = new ClCalculadora();
+            resultat = kal.QuantesVocals(cadena);
+            Assert.AreEqual(numV, resultat);
+            Console.WriteLine("resultatt: " + resultat);
+        }
+
+        [TestMethod]
+        public void QuantesConsonants()
+        {
+            int resultat;
+            String cadena = "hola tonto";
+            int numConsonants = 4;
+
+            ClCalculadora kal = new ClCalculadora();
+            resultat = kal.QuantesConsonants(cadena);
+            Assert.AreEqual(numConsonants, resultat);
+            Console.WriteLine("resultatt: " + resultat);
         }
     }
 }
